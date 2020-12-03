2cfca7914fe5c7460e400b462822da3efc3541db
nmm131 <nmm131@gmail.com>
2020-12-03 14:20:57 -0500

    Update src

--- src/main/string_evaluator.py
+++ src/main/string_evaluator.py
@@ -1,13 +1,13 @@
 # Created by Leon Hunter at 3:57 PM 10/23/2020
 class StringManipulator(object):
     def get_hello_world(self):
-        return None # TODO - Implement solution
+        return "Hello World"
 
     def concatenate(self, value_to_be_added_to, value_to_add):
-        return None # TODO - Implement solution
+        return value_to_be_added_to + value_to_add
 
     def substring_inclusive(self, string_to_fetch_from, starting_index, ending_index):
-        return None # TODO - Implement solution
+        return
 
     def substring_exclusive(self, string_to_fetch_from, starting_index, ending_index):
         return None # TODO - Implement solution
